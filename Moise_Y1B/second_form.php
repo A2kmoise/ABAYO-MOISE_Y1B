214a8f459868dd3176504b0b4906bb9928de0d31
ABAYO-Moise2k <abayomoise950@gmail.com>
2024-11-21 22:56:18 -0800

    Update second_form.php

--- Moise_Y1B/second_form.php
+++ Moise_Y1B/second_form.php
@@ -10,6 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $month = $_POST['month'];
     $range = $_POST['range'];
     $color = $_POST['color'];
+    $study = $_POST['study'];
     $comments = $_POST['comments'];
 
     echo "<h3>Form Submission Details</h3>";
@@ -19,6 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<tr><td>Month of Study</td><td>$month</td></tr>";
     echo "<tr><td>Range</td><td>$range</td></tr>";
     echo "<tr><td>Color</td><td><span style='background-color: $color; padding: 5px;'>$color</span></td></tr>";
+    echo "<tr><td>session</td><td>$study</td></tr>";
     echo "<tr><td>lessons</td><td>$comments</td></tr>";
     echo "</table>";
 }
